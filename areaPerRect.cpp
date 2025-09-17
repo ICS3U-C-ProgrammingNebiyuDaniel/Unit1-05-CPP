7ae083c7970c1ee475104206a0221d49913895fc
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-09-17 18:08:46 +0000

    done part 2

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Created by: Nebiyu Daniel
+// Created on: Sept. 17, 2025
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 5cm X 3cm.
+// Copyright (c) 2025 Nebiyu Daniel. All rights reserved.
+
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 9cm and width 2cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 9 * 2 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (9 + 2) << " cm\n";
+}
